7d0d52bc68531d0123b47cd02edd3719905ffe0e
Andrzej Trzaska <atrzaska2@gmail.com>
2017-07-24 23:39:29 +0200

    Wip import more modules

--- lib/noesis/__init__.py
+++ lib/noesis/__init__.py
@@ -1,28 +1,130 @@
 from Noesis import Noesis
 
-RPGEODATA_FLOAT = 4
-RPGEODATA_INT = 4
-RPGEODATA_USHORT = 2
+BLITFLAG_ALPHABLEND = 'BLITFLAG_ALPHABLEND' # TODO: check value
+FOURCC_ATI1 = 'FOURCC_ATI1' # TODO: check value
+FOURCC_ATI2 = 'FOURCC_ATI2' # TODO: check value
+FOURCC_DXT1 = 'FOURCC_DXT1' # TODO: check value
+FOURCC_DXT1NORMAL = 'FOURCC_DXT1NORMAL' # TODO: check value
+NFORMATFLAG_IMGREAD = 'NFORMATFLAG_IMGREAD' # TODO: check value
+NFORMATFLAG_MODELREAD = 'NFORMATFLAG_MODELREAD' # TODO: check value
+NMATFLAG_ENV_FLIP = 'NMATFLAG_ENV_FLIP' # TODO: check value
+NMATFLAG_PBR_METAL = 'NMATFLAG_PBR_METAL' # TODO: check value
+NMATFLAG_PBR_ROUGHNESS_NRMALPHA = 'NMATFLAG_PBR_ROUGHNESS_NRMALPHA' # TODO: check value
+NMATFLAG_PBR_SPEC_IR_RG = 'NMATFLAG_PBR_SPEC_IR_RG' # TODO: check value
+NMATFLAG_TWOSIDED = 'NMATFLAG_TWOSIDED' # TODO: check value
+NOE_ENCODEDXT_BC1 = 'NOE_ENCODEDXT_BC1' # TODO: check value
+NOEKF_INTERPOLATE_LINEAR = 'NOEKF_INTERPOLATE_LINEAR' # TODO: check value
+NOEKF_ROTATION_QUATERNION_4 = 'NOEKF_ROTATION_QUATERNION_4' # TODO: check value
+NOEKF_SCALE_SCALAR_1 = 'NOEKF_SCALE_SCALAR_1' # TODO: check value
+NOEKF_TRANSLATION_VECTOR_3 = 'NOEKF_TRANSLATION_VECTOR_3' # TODO: check value
+NOESISTEX_DXT1 = 'NOESISTEX_DXT1' # TODO: check value
+NOESISTEX_DXT3 = 'NOESISTEX_DXT3' # TODO: check value
+NOESISTEX_DXT5 = 'NOESISTEX_DXT5' # TODO: check value
+NOESISTEX_RGB24 = 'NOESISTEX_RGB24' # TODO: check value
+NOESISTEX_RGBA32 = 'NOESISTEX_RGBA32' # TODO: check value
+NOESISTEX_UNKNOWN = 'NOESISTEX_UNKNOWN' # TODO: check value
+NOEUSERVAL_SAVEFILEPATH = 'NOEUSERVAL_SAVEFILEPATH' # TODO: check value
+NOEUSERVAL_STRING = 'NOEUSERVAL_STRING' # TODO: check value
+NTEXFLAG_CUBEMAP = 'NTEXFLAG_CUBEMAP' # TODO: check value
+NTEXFLAG_FILTER_NEAREST = 'NTEXFLAG_FILTER_NEAREST' # TODO: check value
+NTEXFLAG_WRAP_CLAMP = 'NTEXFLAG_WRAP_CLAMP' # TODO: check value
+NTOOLFLAG_CONTEXTITEM = 'NTOOLFLAG_CONTEXTITEM' # TODO: check value
+OPTFLAG_WANTARG = 'OPTFLAG_WANTARG' # TODO: check value
+PVRTC_DECODE_LINEARORDER = 'PVRTC_DECODE_LINEARORDER' # TODO: check value
+PVRTC_DECODE_PVRTC2 = 'PVRTC_DECODE_PVRTC2' # TODO: check value
+PVRTC_DECODE_PVRTC2_NO_OR_WITH_0_ALPHA = 'PVRTC_DECODE_PVRTC2_NO_OR_WITH_0_ALPHA' # TODO: check value
+RPGEO_QUAD_ABC_ACD = 'RPGEO_QUAD_ABC_ACD' # TODO: check value
 RPGEO_TRIANGLE = 'RPGEO_TRIANGLE' # TODO: check value
 RPGEO_TRIANGLE_STRIP = 'RPGEO_TRIANGLE_STRIP' # TODO: check value
-RPGEO_POLYGON = 'RPGEO_POLYGON' # TODO: check value
-NOESISTEX_RGBA32 = 'NOESISTEX_RGBA32' # TODO: check value
-NOEKF_ROTATION_QUATERNION_4 = 'NOEKF_ROTATION_QUATERNION_4' # TODO: check value
-NOEKF_INTERPOLATE_LINEAR = 'NOEKF_INTERPOLATE_LINEAR' # TODO: check value
-NOEKF_TRANSLATION_VECTOR_3 = 'NOEKF_TRANSLATION_VECTOR_3' # TODO: check value
-NOEKF_SCALE_SCALAR_1 = 'NOEKF_SCALE_SCALAR_1' # TODO: check value
-OPTFLAG_WANTARG = 'OPTFLAG_WANTARG' # TODO: check value
-NMATFLAG_PBR_METAL = 'NMATFLAG_PBR_METAL' # TODO: check value (flag)
-NMATFLAG_PBR_SPEC_IR_RG = 'NMATFLAG_PBR_SPEC_IR_RG' # TODO: check value (flag)
-
+RPGEODATA_BYTE = 1
+RPGEODATA_FLOAT = 4
+RPGEODATA_HALFFLOAT = 2
+RPGEODATA_INT = 4
+RPGEODATA_SHORT = 2
+RPGEODATA_UBYTE = 1
+RPGEODATA_UINT = 4
+RPGEODATA_USHORT = 2
+RPGOPT_BIGENDIAN = 'RPGOPT_BIGENDIAN' # TODO: check value
+RPGOPT_SWAPHANDEDNESS = 'RPGOPT_SWAPHANDEDNESS' # TODO: check value
+RPGOPT_TANMATROTATE = 'RPGOPT_TANMATROTATE' # TODO: check value
+RPGOPT_TRIWINDBACKWARD = 'RPGOPT_TRIWINDBACKWARD' # TODO: check value
 
 noesis = Noesis()
+
+def addOption(handle, option, description, flags):
+    return noesis.addOption(handle, option, description, flags)
+
+def allocBytes():
+    return noesis.allocBytes()
+
+def doException():
+    return noesis.doException()
+
+def freeModule():
+    return noesis.freeModule()
+
+def g_flRadToDeg():
+    return noesis.g_flRadToDeg()
+
+def getCharSplineSet():
+    return noesis.getCharSplineSet()
+
+def getFormatExtensionFlags():
+    return noesis.getFormatExtensionFlags()
+
+def getMFFP(uint):
+    return noesis.getMFFP(uint)
+
+def getScenesPath():
+    return noesis.getScenesPath()
+
+def getSelectedDirectory():
+    return noesis.getSelectedDirectory()
+
+def getSelectedFile():
+    return noesis.getSelectedFile()
+
+def getWindowHandle():
+    return noesis.getWindowHandle()
+
+def instantiateModule():
+    return noesis.instantiateModule()
+
+def isPreviewModuleRAPIValid():
+    return noesis.isPreviewModuleRAPIValid()
+
+def loadImageRGBA():
+    return noesis.loadImageRGBA()
+
+def logPopup():
+    return noesis.logPopup()
+
+def messagePrompt():
+    return noesis.messagePrompt()
+
+def openAndRemoveTempFile():
+    return noesis.openAndRemoveTempFile()
+
+def openFile():
+    return noesis.openFile()
+
+def optGetArg():
+    return noesis.optGetArg()
+
+def optWasInvoked():
+    return noesis.optWasInvoked()
 
 def register(name, fileType):
     return noesis.register(name, fileType)
 
-def setHandlerTypeCheck(handle, noepyCheckType):
-    return noesis.setHandlerTypeCheck(handle, noepyCheckType)
+def registerCleanupFunction():
+    return noesis.registerCleanupFunction()
+
+def registerTool():
+    return noesis.registerTool()
+
+def setHandlerExtractArc(handle, grpExtractArc):
+    return noesis.setHandlerExtractArc(handle, grpExtractArc)
 
 def setHandlerLoadModel(handle, noepyLoadModel):
     return noesis.setHandlerLoadModel(handle, noepyLoadModel)
@@ -30,17 +132,29 @@ def setHandlerLoadModel(handle, noepyLoadModel):
 def setHandlerLoadRGBA(handle, dsgLoadRGBA):
     return noesis.setHandlerLoadRGBA(handle, dsgLoadRGBA)
 
-def setHandlerExtractArc(handle, grpExtractArc):
-    return noesis.setHandlerExtractArc(handle, grpExtractArc)
+def setHandlerTypeCheck(handle, noepyCheckType):
+    return noesis.setHandlerTypeCheck(handle, noepyCheckType)
+
+def setHandlerWriteModel(self, handle, noepyWriteModel):
+    return noesis.setHandlerWriteModel(handle, noepyWriteModel)
 
 def setHandlerWriteRGBA(handle, walWriteRGBA):
     return noesis.setHandlerWriteRGBA(handle, walWriteRGBA)
 
-def allocType(type, data):
-    return noesis.allocType(type, data)
+def setHandlerWriteAnim(handle, noepyWriteAnim):
+    return noesis.setHandlerWriteAnim(handle, noepyWriteAnim)
 
-def logPopup():
-    return noesis.logPopup()
+def setModuleRAPI():
+    return noesis.setModuleRAPI()
 
-def addOption(handle, option, description, flags):
-    return noesis.addOption(handle, option, description, flags)
+def setPreviewModuleRAPI():
+    return noesis.setPreviewModuleRAPI()
+
+def setToolFlags():
+    return noesis.setToolFlags()
+
+def setToolVisibleCallback():
+    return noesis.setToolVisibleCallback()
+
+def userPrompt():
+    return noesis.userPrompt()
--- lib/noesis/noesis.py
+++ lib/noesis/noesis.py
@@ -5,45 +5,35 @@ class Noesis:
         self.plugins = []
         self.mdlList = []
 
-    def register(self, name, fileType):
-        handler = Handler(name, fileType)
-        self.plugins.append(handler)
-        return handler
-
-    def setHandlerTypeCheck(self, handle, noepyCheckType):
-        handle.noepyCheckType = noepyCheckType
-
-    def setHandlerLoadModel(self, handle, noepyLoadModel):
-        handle.noepyLoadModel = noepyLoadModel
-
-    def setHandlerWriteModel(self, handle, noepyWriteModel):
-        handle.noepyWriteModel = noepyWriteModel
-
-    def setHandlerLoadRGBA(self, handle, txdLoadRGBA):
-        handle.txdLoadRGBA = txdLoadRGBA
-
-    def setHandlerWriteRGBA(self, handle, walWriteRGBA):
-        handle.walWriteRGBA = walWriteRGBA
-
-    def setHandlerWriteAnim(self, handle, noepyWriteAnim):
-        handle.noepyWriteAnim = noepyWriteAnim
-
-    def setHandlerExtractArc(self, handle, grpExtractArc):
-        handle.grpExtractArc = grpExtractArc
-
-    def allocType(self, type, data):
-        # TODO: implement me
-        pass
-
-    def logPopup(self):
-        # TODO: implement me
-        pass
-
     def addOption(self, handle, option, description, flags):
         # TODO: implement me
         pass
 
-    def getMFFP(uint):
+    def allocBytes(self):
+        # TODO: implement me
+        pass
+
+    def doException(self):
+        # TODO: implement me
+        pass
+
+    def freeModule(self):
+        # TODO: implement me
+        pass
+
+    def g_flRadToDeg(self):
+        # TODO: implement me
+        pass
+
+    def getCharSplineSet(self):
+        # TODO: implement me
+        pass
+
+    def getFormatExtensionFlags(self):
+        # TODO: implement me
+        pass
+
+    def getMFFP(self, uint):
         # TODO: implement me
         pass
 
@@ -51,6 +41,104 @@ class Noesis:
         # TODO: implement me
         pass
 
-    def optWasInvoked(self, option):
-        # tODO: implement me
+    def getSelectedDirectory(self):
+        # TODO: implement me
+        pass
+
+    def getSelectedFile(self):
+        # TODO: implement me
+        pass
+
+    def getWindowHandle(self):
+        # TODO: implement me
+        pass
+
+    def instantiateModule(self):
+        # TODO: implement me
+        pass
+
+    def isPreviewModuleRAPIValid(self):
+        # TODO: implement me
+        pass
+
+    def loadImageRGBA(self):
+        # TODO: implement me
+        pass
+
+    def logPopup(self):
+        # TODO: implement me
+        pass
+
+    def messagePrompt(self):
+        # TODO: implement me
+        pass
+
+    def openAndRemoveTempFile(self):
+        # TODO: implement me
+        pass
+
+    def openFile(self):
+        # TODO: implement me
+        pass
+
+    def optGetArg(self):
+        # TODO: implement me
+        pass
+
+    def optWasInvoked(self):
+        # TODO: implement me
         return true
+
+    def register(self, name, fileType):
+        handler = Handler(name, fileType)
+        self.plugins.append(handler)
+        return handler
+
+    def registerCleanupFunction(self):
+        # TODO: implement me
+        pass
+
+    def registerTool(self):
+        # TODO: implement me
+        pass
+
+    def setHandlerExtractArc(self, handle, grpExtractArc):
+        handle.grpExtractArc = grpExtractArc
+
+    def setHandlerLoadModel(self, handle, noepyLoadModel):
+        handle.noepyLoadModel = noepyLoadModel
+
+    def setHandlerLoadRGBA(self, handle, txdLoadRGBA):
+        handle.txdLoadRGBA = txdLoadRGBA
+
+    def setHandlerTypeCheck(self, handle, noepyCheckType):
+        handle.noepyCheckType = noepyCheckType
+
+    def setHandlerWriteModel(self, handle, noepyWriteModel):
+        handle.noepyWriteModel = noepyWriteModel
+
+    def setHandlerWriteRGBA(self, handle, walWriteRGBA):
+        handle.walWriteRGBA = walWriteRGBA
+
+    def setHandlerWriteAnim(self, handle, noepyWriteAnim):
+        handle.noepyWriteAnim = noepyWriteAnim
+
+    def setModuleRAPI(self):
+        # TODO: implement me
+        pass
+
+    def setPreviewModuleRAPI(self):
+        # TODO: implement me
+        pass
+
+    def setToolFlags(self):
+        # TODO: implement me
+        pass
+
+    def setToolVisibleCallback(self):
+        # TODO: implement me
+        pass
+
+    def userPrompt(self):
+        # TODO: implement me
+        pass
