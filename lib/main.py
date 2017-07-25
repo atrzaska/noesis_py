7a1a1358a1710d3cec4815f811d89ba1feb4a166
Andrzej Trzaska <atrzaska2@gmail.com>
2017-07-25 13:03:11 +0200

    Move pymmd

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,5 @@
 import sys
-from PyMmdApplication import PyMmdApplication
 from NoesisApplication import NoesisApplication
 
 if __name__ == "__main__":
-    # PyMmdApplication().run(sys.argv[1])
     NoesisApplication().run()
