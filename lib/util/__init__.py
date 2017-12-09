fb9b83ebb91bbbb900c18a812b039f14f00b6af0
Andrzej Trzaska <atrzaska2@gmail.com>
2017-12-09 22:08:28 +0100

    Add missing FOURCC codes and dump method

--- lib/util/__init__.py
+++ lib/util/__init__.py
@@ -6,3 +6,8 @@ def logNotImplementedMethod(name, args):
 
 def last(arr):
     return arr[-1] if len(arr) > 0 else None
+
+def dump(path, data):
+    handle = open(path, 'wb')
+    handle.write(data)
+    handle.close()
